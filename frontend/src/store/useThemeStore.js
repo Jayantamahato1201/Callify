3e84f6cdcab45339da88e53f1be672cb4a827832
Jayantamahato1201 <124423161+Jayantamahato1201@users.noreply.github.com>
2026-05-31 14:54:23 +0530

    cc

--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("Callify-theme") || "coffee",
-  setTheme: (theme) => {
-    localStorage.setItem("Callify-theme", theme);
-    set({ theme });
-  },
-}));
